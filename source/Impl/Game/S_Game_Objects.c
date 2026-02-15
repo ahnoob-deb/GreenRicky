9d126b405f7293cf7c5a0fc51194c88ed43717b9
a.h.noob <a.h.noob@hotmail.com>
2026-02-15 01:01:23 +0100

    updated documentation + Cleaned up S_Game_Objects.c and changed the order of some functions so that gcc requires a prototype for static functions.

--- source/Impl/Game/S_Game_Objects.c
+++ source/Impl/Game/S_Game_Objects.c
@@ -1,9 +1,7 @@
-#include "S_Game_Objects.h"
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
- 
+
+#include "S_Game_Objects.h"
 #include "D_Common.h"
 #include "R_Resources.h"
 #include "S_Quit.h"
@@ -22,29 +20,24 @@ char *gal_shape_ids[COUNT_SHAPES] = {
 
 /* load a shape into the shape-tree */
 static void gal_load_shape(const int *p_shape, char *p_id);
-
+/* calculate the dimensions and where a piece's shape actually is located in the matrix. */
 static void gal_shape_update_dimensions(ShapeData_t *p_shape);
 
+/* where a piece's shape actually is located in the matrix. */
 static void gal_shape_calc_min_x(ShapeData_t *p_sd);
 static void gal_shape_calc_min_y(ShapeData_t *p_sd);
-
 static void gal_shape_calc_max_x(ShapeData_t *p_sd);
 static void gal_shape_calc_max_y(ShapeData_t *p_sd);
 
+/* ensure, a piece stays in the map. */
 static void gal_piece_check_map_bounds(Piece_t *p_pce);
 
+/* checks if a given shape with id p_id can be found in the shape tree. */
 static void gal_shape_check_node(char *p_id);
 
 /* allocate memory for a Piece. */
 static Piece_t *gal_alloc_piece(void);
 
-/* Allocate memory for a Piece. This will return a pointer to the Piece or NULL
- * in case of error.
- */
-static Piece_t *gal_alloc_piece(void)
-{
-    return (Piece_t *)malloc(sizeof(Piece_t));
-}
 
 /* loads all shapes needed in the game into the tree and checks, wether
  * they can be found.
@@ -175,6 +168,15 @@ Piece_t *gal_piece_new(const unsigned int p_x, const unsigned int p_y,
     return pce;
 }
 
+/* Allocate memory for a Piece. This will return a pointer to the Piece or NULL
+ * in case of error.
+ */
+static Piece_t *gal_alloc_piece(void)
+{
+    return (Piece_t *)malloc(sizeof(Piece_t));
+}
+
+
 void gal_piece_init(Piece_t *pce, const unsigned int p_x, const unsigned int p_y,
                 unsigned int p_color,
                 char *p_shape_id, const int p_dir)
@@ -206,26 +208,7 @@ void gal_piece_delete(Piece_t *p_pce)
     free(p_pce);
 }
 
-
-/* Ensure the Piece stays within the boundaries of the game area */
-static void gal_piece_check_map_bounds(Piece_t *p_pce)
-{
-    ShapeData_t *sh = p_pce->sh_data[p_pce->direction];
-
-    /* Prevent moving left out of bounds */
-    if (p_pce->x < 0)
-        p_pce->x = 0;
-    /* Prevent moving up out of bounds */
-    if (p_pce->y < 0)
-        p_pce->y = 0;
-    /* Prevent moving right out of bounds */
-    if (p_pce->x + sh->width >= MAP_WIDTH)
-        p_pce->x = (MAP_WIDTH - 1) - sh->width;
-    /* Prevent moving down out of bounds */
-    if (p_pce->y + sh->height >= MAP_HEIGHT)
-        p_pce->y = (MAP_HEIGHT - 1) - sh->height;
-}
-
+/* calculate the value of the next direction. */
 int gal_piece_next_direction(Piece_t *p_pce)
 {
     int dir = p_pce->direction;
@@ -270,126 +253,27 @@ void gal_piece_move(Piece_t *p_pce, double p_dx, double p_dy)
     gal_piece_check_map_bounds(p_pce);
 }
 
+/* Ensure the Piece stays within the boundaries of the game area */
+static void gal_piece_check_map_bounds(Piece_t *p_pce)
+{
+    ShapeData_t *sh = p_pce->sh_data[p_pce->direction];
+
+    /* Prevent moving left out of bounds */
+    if (p_pce->x < 0)
+        p_pce->x = 0;
+    /* Prevent moving up out of bounds */
+    if (p_pce->y < 0)
+        p_pce->y = 0;
+    /* Prevent moving right out of bounds */
+    if (p_pce->x + sh->width >= MAP_WIDTH)
+        p_pce->x = (MAP_WIDTH - 1) - sh->width;
+    /* Prevent moving down out of bounds */
+    if (p_pce->y + sh->height >= MAP_HEIGHT)
+        p_pce->y = (MAP_HEIGHT - 1) - sh->height;
+}
+
+
 /* SHAPE */
-static void gal_shape_update_dimensions(ShapeData_t *p_shape)
-{
-
-    gal_shape_calc_min_x(p_shape);
-    gal_shape_calc_min_y(p_shape);
-
-    gal_shape_calc_max_x(p_shape);
-    gal_shape_calc_max_y(p_shape);
-
-    p_shape->width = p_shape->max_x - p_shape->min_x;
-    p_shape->height = p_shape->max_y - p_shape->min_y;
-
-}
-
-static void gal_shape_calc_min_x(ShapeData_t *p_sd)
-{
-    int first_block_x = 0;
-    unsigned short found = 0;
-
-    int x = 0;
-    int y = 0;
-
-    for (x = 0; x < PIECE_WIDTH; x++)
-    {
-
-        for (y = 0; y < PIECE_HEIGHT && !found; y++)
-        {
-
-            if (p_sd->matrix[y * PIECE_WIDTH + x] > FREE)
-            {
-                first_block_x = x;
-                found = 1;
-                break; /* first occurrence - and out... */
-            }
-        }
-    }
-    p_sd->min_x = first_block_x;
-}
-
-static void gal_shape_calc_min_y(ShapeData_t *p_sd)
-{
-    int first_block_y = 0;
-    unsigned short found = 0;
-
-    int y = 0;
-    int x = 0;
-
-    for (y = 0; y < PIECE_HEIGHT && !found; y++)
-    {
-        for (x = 0; x < PIECE_WIDTH; x++)
-        {
-
-            if (p_sd->matrix[y * PIECE_WIDTH + x] > FREE)
-            {
-                first_block_y = y;
-                found = 1;
-                break; /* first occurrence - and out... */
-            }
-        }
-    }
-    p_sd->min_y = first_block_y;
-}
-
-static void gal_shape_calc_max_x(ShapeData_t *p_sd)
-{
-
-    /* Following we have to find out, where from the right side
-       starting the first BLOCK can be found. This is needed in
-       first place in AntrisPiece.move() for border collision check. */
-
-    unsigned short found = 0;
-    int first_block_x = 0;
-
-    int x = 0;
-    int y = 0;
-
-    for (x = PIECE_WIDTH - 1; x >= 0 && !found; x--)
-    {
-
-        for (y = 0; y < PIECE_HEIGHT; y++)
-        {
-
-            if (p_sd->matrix[y * PIECE_WIDTH + x] > FREE)
-            {
-                first_block_x = x;
-                found = 1;
-                break; /* first occurrence - and out... */
-            }
-        }
-    }
-    p_sd->max_x = first_block_x;
-}
-
-static void gal_shape_calc_max_y(ShapeData_t *p_sd)
-{
-
-    /* Following we have to find out, in which line
-       the first BLOCK is found.
-       Because a Piece may have a FREE last line,
-       the calculation for the borders check in AntrisPiece.move
-       would deliver wrong values for hitting the ground, if
-       last line of current_shape is free. */
-
-    int first_block_y = 0;
-
-    int index = 0;
-
-    for (index = PIECE_WIDTH * PIECE_HEIGHT - 1; index >= 0; index--)
-    {
-        if (p_sd->matrix[index] > FREE)
-        {
-            first_block_y = index / PIECE_WIDTH;
-            break; /* first occurrence - and out... */
-        }
-    }
-
-    p_sd->max_y = first_block_y;
-}
-
 /* load a shape into the shape-tree */
 static void gal_load_shape(const int *p_shape, char *p_id)
 {
@@ -442,17 +326,139 @@ static void gal_load_shape(const int *p_shape, char *p_id)
     add_shape(p_id, sd);
 }
 
+/* calculate the key data of a shape. */
+static void gal_shape_update_dimensions(ShapeData_t *p_shape)
+{
+
+    gal_shape_calc_min_x(p_shape);
+    gal_shape_calc_min_y(p_shape);
+
+    gal_shape_calc_max_x(p_shape);
+    gal_shape_calc_max_y(p_shape);
+
+    p_shape->width = p_shape->max_x - p_shape->min_x;
+    p_shape->height = p_shape->max_y - p_shape->min_y;
+
+}
+
+/* where a piece's shape actually is located in the matrix. */
+static void gal_shape_calc_min_x(ShapeData_t *p_sd)
+{
+    int first_block_x = 0;
+    unsigned short found = 0;
+
+    int x = 0;
+    int y = 0;
+
+    for (x = 0; x < PIECE_WIDTH; x++)
+    {
+
+        for (y = 0; y < PIECE_HEIGHT && !found; y++)
+        {
+
+            if (p_sd->matrix[y * PIECE_WIDTH + x] > FREE)
+            {
+                first_block_x = x;
+                found = 1;
+                break; /* first occurrence - and out... */
+            }
+        }
+    }
+    p_sd->min_x = first_block_x;
+}
+
+/* where a piece's shape actually is located in the matrix. */
+static void gal_shape_calc_min_y(ShapeData_t *p_sd)
+{
+    int first_block_y = 0;
+    unsigned short found = 0;
+
+    int y = 0;
+    int x = 0;
+
+    for (y = 0; y < PIECE_HEIGHT && !found; y++)
+    {
+        for (x = 0; x < PIECE_WIDTH; x++)
+        {
+
+            if (p_sd->matrix[y * PIECE_WIDTH + x] > FREE)
+            {
+                first_block_y = y;
+                found = 1;
+                break; /* first occurrence - and out... */
+            }
+        }
+    }
+    p_sd->min_y = first_block_y;
+}
+
+/* where a piece's shape actually is located in the matrix. */
+static void gal_shape_calc_max_x(ShapeData_t *p_sd)
+{
+
+    /* Following we have to find out, where from the right side
+       starting the first BLOCK can be found. This is needed in
+       first place in AntrisPiece.move() for border collision check. */
+
+    unsigned short found = 0;
+    int first_block_x = 0;
+
+    int x = 0;
+    int y = 0;
+
+    for (x = PIECE_WIDTH - 1; x >= 0 && !found; x--)
+    {
+
+        for (y = 0; y < PIECE_HEIGHT; y++)
+        {
+
+            if (p_sd->matrix[y * PIECE_WIDTH + x] > FREE)
+            {
+                first_block_x = x;
+                found = 1;
+                break; /* first occurrence - and out... */
+            }
+        }
+    }
+    p_sd->max_x = first_block_x;
+}
+
+/* where a piece's shape actually is located in the matrix. */
+static void gal_shape_calc_max_y(ShapeData_t *p_sd)
+{
+
+    /* Following we have to find out, in which line
+       the first BLOCK is found.
+       Because a Piece may have a FREE last line,
+       the calculation for the borders check in AntrisPiece.move
+       would deliver wrong values for hitting the ground, if
+       last line of current_shape is free. */
+
+    int first_block_y = 0;
+
+    int index = 0;
+
+    for (index = PIECE_WIDTH * PIECE_HEIGHT - 1; index >= 0; index--)
+    {
+        if (p_sd->matrix[index] > FREE)
+        {
+            first_block_y = index / PIECE_WIDTH;
+            break; /* first occurrence - and out... */
+        }
+    }
+
+    p_sd->max_y = first_block_y;
+}
+
+
+
 /*  print shape data to console, just for verification here. */
 void gal_shape_print(ShapeData_t **p_shape)
 {
-
     printf("print shape : \n");
-
     int size_matrix = PIECE_WIDTH * PIECE_HEIGHT;
-
     int shape_number = 0;
     int index = 0;
-
     for (shape_number = 0;
          shape_number < COUNT_DIRECTIONS;
          shape_number++)
